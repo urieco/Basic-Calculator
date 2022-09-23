cbb2602687e743dd96a36f64e166c7b11f7e74d1
urieco <urieco@github.com>
2022-09-23 18:09:03 +0700

    Get the calculator to process one equation

--- README.md
+++ README.md
@@ -15,12 +15,14 @@ General Outline:
 3. Create a basic layout for the HTML calculator with buttons for each digit, each of the above functions and "Equals" key. 
     - Add a "clear" button.
 
-**To Do**
+
 
 4. Create a function that populate the display when you click the number buttons. You should be storing the 'display' value' in a variable somewhere for use in the next step. 
 
 5. Wire the operate() with "=" key. 
     - Update the display with result.
+
+**To Do**
 
 6. User should be able to string together several operations and get the right answer, with **each pair of numbers being evaluated at a time**. 
     - Pressing any more operator key after the input of two operands and their operator will result in operate() being executed. 
@@ -38,3 +40,4 @@ Extra Credit:
 Future plans: 
 - Instead of throwing an error for large numbers, use 10^n to express the result, round up numbers with a lot of decimals.
 - Calculator is capable of solving functions with more than two operands. 
+    + Numbers get smaller as you input above the limit of 9 digits
--- script.js
+++ script.js
@@ -18,6 +18,115 @@ function playSound() {
     audio.play();
 }
 
+let num1 = 0;
+let num2 = 0;
+let result = 0;
+let gotOperator = false;
+
+const displayMonitor = document.querySelector("#display-monitor");
+const previousDisplay = document.querySelector(".previous-display");
+const currentDisplay = document.querySelector(".current-display");
+const operator = document.querySelector(".operator");
+
+displayMonitor.appendChild(previousDisplay);
+displayMonitor.appendChild(currentDisplay);
+displayMonitor.appendChild(operator);
+
+const numpad = document.querySelectorAll(".one, .two, .three, .four, .five, .six, .seven, .eight, .nine, .zero, .decimal");
+numpad.forEach(numKey => numKey.addEventListener("click", inputNumber));
+
+//Decide whether to get to the input of num2 or not
+function inputNumber() {
+    const goToNum2 = new Promise((resolve) => {
+        if (num1 && gotOperator) {
+            currentDisplay.textContent = ""
+            resolve(this.textContent);
+        }
+    });
+
+    goToNum2
+        .then((value) => currentDisplay.text += value)
+        .then(() => gotOperator = false)
+        .catch(() => console.error("Error"))
+
+    if (currentDisplay.textContent.length < 10) currentDisplay.textContent += this.textContent;
+    // if (result) {
+    //     num1 = result;
+    //     num2 = 0;
+    // }
+}
+
+
+//Pressing the math symbol will also register num1's value
+const symbols = document.querySelectorAll(".divide, .multiply, .subtract, .add");
+symbols.forEach(symbol => symbol.addEventListener("click", getOperator));
+
+function getOperator() {
+    if (currentDisplay.textContent == "" && this.textContent == "-") {
+        currentDisplay.textContent += "-";
+    }
+    if (currentDisplay.textContent == "" || currentDisplay.textContent == "-") return;
+    operator.textContent = this.textContent;
+    num1 = +currentDisplay.textContent;
+    gotOperator = true;
+}
+
+
+//Pressing the equal symbol will register num2 and output the answer
+const equalBtn = document.querySelector(".equal");
+equalBtn.addEventListener("click", getResult);
+
+function getResult() {
+    if (!num1 || !operator.textContent) return;
+
+    if (num1 && !num2 && gotOperator) {
+        currentDisplay.textContent = num1;
+        operator.textContent = "";
+    }
+    if (num1 && operator.textContent && !gotOperator) {
+        num2 = +currentDisplay.textContent;
+
+        const goToResult = new Promise((resolve, reject) => {
+            if (num2) {
+                currentDisplay.textContent = "";
+                resolve(num2);
+            }
+            if (num2 == 0 && operator.textContent == "/") {
+                reject("Error");
+            }
+        });
+        goToResult
+            .then((num2) => result = operate(num1, operator.textContent, num2))
+            .then(() => operator.textContent = "")
+            .then(() => currentDisplay.textContent = result)
+            .catch((warning) => currentDisplay.textContent = warning);
+    }
+}
+
+
+
+//Clear screen and Backspace
+const backspace = document.querySelector(".backspace");
+backspace.addEventListener("click", doBackspace);
+
+function doBackspace() {
+    currentDisplay.textContent = currentDisplay.textContent.slice(0, currentDisplay.textContent.length - 1);
+}
+
+const clearBtn = document.querySelector(".clear");
+clearBtn.addEventListener("click", clearScreen);
+
+function clearScreen() {
+    currentDisplay.textContent = "";
+    previousDisplay.textContent = "";
+    operator.textContent = "";
+    num1 = 0;
+    num2 = 0;
+    result = 0;
+}
+
+
+//Basic Math Functions
 const addition = function (num1, num2) {
     return num1 + num2;
 };
@@ -31,22 +140,22 @@ const multiplication = function (num1, num2) {
 };
 
 const division = function (num1, num2) {
-    if (num2 === 0) return "Error! This will detonate the bomb. Goodbye.";
+    if (num2 === 0) return "Error!";
     return num1 / num2;
 };
 
 const operate = function (num1, operator, num2) {
     switch (operator) {
-        case "add":
+        case "+":
             return addition(num1, num2);
             break;
-        case "subtract":
+        case "-":
             return subtraction(num1, num2);
             break;
-        case "multiply":
+        case "X":
             return multiplication(num1, num2);
             break;
-        case "divide":
+        case "/":
             return division(num1, num2);
             break;
     }
